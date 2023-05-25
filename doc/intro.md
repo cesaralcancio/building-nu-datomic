e56d387dca98283e796144d656a022c407349074
Cesar Alcancio <cesar.alcancio@gmail.com>
2023-05-25 20:21:19 -0300

    Create the project using lein new building-nu-datomic

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to building-nu-datomic
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/building_nu_datomic/core_test.clj
@@ -0,0 +1,7 @@
+(ns building-nu-datomic.core-test
+  (:require [clojure.test :refer :all]
+            [building-nu-datomic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
