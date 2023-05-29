32560225d43e1623180ed7327de04681b7963314
Cesar Alcancio <cesar.alcancio@gmail.com>
2023-05-28 21:43:10 -0300

    Add the Datoms examples

--- src/building_nu_datomic/example_1_schema.clj
+++ src/building_nu_datomic/example_1_schema.clj
@@ -1,4 +1,22 @@
 (ns building-nu-datomic.example-1-schema
   (:require [building-nu-datomic.datomic-config.config :as config]))
 
+;; Datomic Datom
+;; https://docs.datomic.com/cloud/whatis/data-model.html
+;;
+;; [Entity  Attribute         Value       Transaction   Operation]
+;; [42      :order/id         1           1234          true]
+;; [42      :order/created-at 2023-05-28  1234          true]
+;; [42      :order/value      123.99M     1234          true]
+;; [42      :order/status     :pending    1234          true]
+;; [43      :order/id         2           4567          true]
+;; [43      :order/created-at 2023-05-28  4567          true]
+;; [43      :order/value      123.99M     4567          true]
+;; [43      :order/status     :pending    4567          true]
+;; ...
+;; [Entity    Attribute         Value                 Transaction   Operation]
+;; [1234      :db/txInstant     2023-05-28 01:05:00   1234          true]
+;; [4567      :db/txInstant     2023-05-28 01:10:00   4567          true]
+
+;; Let's define the entities!
 (config/create-db-with-schema)
